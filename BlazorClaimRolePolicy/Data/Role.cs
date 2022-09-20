bebf25fd86d4302e4f7770dedf32a34ab8c41af0
Nima Zare <NimaDeveloper@yahoo.com>
2022-09-20 13:01:51 +0430

    first commit

---
+++ BlazorClaimRolePolicy/Data/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorClaimRolePolicy.Data
+{
+    public class Role
+    {
+        public String RoleName { get; set; } = String.Empty;
+        public String ID { get; set; } = String.Empty;
+    }
+}
---
+++ BlazorClaimRolePolicy/Data/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorClaimRolePolicy.Data
+{
+    public class User
+    {
+        [EmailAddress]
+        [Required]
+        public String LoginName { get; set; } = String.Empty;
+        [Required]
+        public String UserRole { get; set; } = String.Empty;
+    }
+}
